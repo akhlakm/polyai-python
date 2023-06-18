c241a0563c46883ff0c7d6049e805609533976e5
akhlakm <me@akhlakm.com>
2023-06-18 18:33:48 -0400

    Refactor

--- polyai/api/__init__.py
+++ polyai/api/__init__.py
@@ -4,7 +4,7 @@ from .chat_completion import ChatCompletion
 
 api_key = os.environ.get("POLYAI_API_KEY")
 api_key_path = os.environ.get("POLYAI_API_KEY_PATH")
-api_base = os.environ.get("POLYAI_API_BASE", "http://localhost:8080/v1")
+api_base = os.environ.get("POLYAI_API_BASE", "http://localhost:8080/api/")
 
 session = None
 app_info = None
