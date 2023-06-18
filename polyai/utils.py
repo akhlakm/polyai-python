2b3683089d40247c9c9c07dee22ab0c2fbc88d51
akhlakm <me@akhlakm.com>
2023-06-17 21:50:31 -0400

    Read environment vars

---
+++ polyai/utils.py
@@ -0,0 +1,15 @@
+import polyai
+
+def default_api_key() -> str:
+    if polyai.api_key_path:
+        with open(polyai.api_key_path, "rt") as k:
+            api_key = k.read().strip()
+            if not api_key.startswith("pl-"):
+                raise ValueError(f"Malformed API key in {polyai.api_key_path}.")
+            return api_key
+    elif polyai.api_key is not None:
+        return polyai.api_key
+    else:
+        raise polyai.error.AuthenticationError(
+            "No API key provided. You can set your API key in code using 'polyai.api_key = <API-KEY>', or you can set the environment variable POLYAI_API_KEY=<API-KEY>). If your API key is stored in a file, you can point the polyai module at it with 'polyai.api_key_path = <PATH>'."
+        )
