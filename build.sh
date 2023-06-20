0d7564f3b9cc43d1773654a8d64d9ba4cf370c8f
akhlakm <me@akhlakm.com>
2023-06-19 21:17:33 -0400

    Refactor

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ bump() {
     grep version pyproject.toml
     VERSION=$(sed -n 's/version = "\(.*\)"/\1/p' pyproject.toml)
     VERSION=$(python -c "v='$VERSION'.split('.');print('%s.%s.%02d' %(v[0], v[1], int(v[2])+1))")
+    echo "   >>>"
     sed -i "s/\(version = \"\)[^\"]*\"/\1$VERSION\"/" pyproject.toml
     sed -i "s/\(__version__ = \"\)[^\"]*\"/\1$VERSION\"/" polyai/__init__.py
     grep version pyproject.toml
@@ -32,8 +33,7 @@ container() {
     mkdir -p "$POLYAI_SERV_CACHE" "$POLYAI_MODELS"
 
     docker rm polyai > /dev/null
-    docker run -it --gpus all \
-        --network $DB_NETWORK \
+    docker run -it --gpus all --network $DB_NETWORK \
         -v "$POLYAI_SERV_CACHE:/home/user/.cache/" \
         -v "$POLYAI_MODELS:/home/user/models" \
         -p $POLYAI_SERV_PORT:8080 \
@@ -43,7 +43,7 @@ container() {
 apitest() {
     curl    -v --data @request.json \
             --header "Content-Type: application/json" \
-            http://localhost:8080/api/chat/completions
+            https://localhost:8080/api/chat/completions
     echo 
 }
 
