9bae470af5e7b92de21ec89d11b411bc42063665
akhlakm <me@akhlakm.com>
2023-06-20 23:45:13 -0400

    Log vram usage

--- polyai/server/models.py
+++ polyai/server/models.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import time
+import torch
 
 import pylogg
 import polyai.server
@@ -20,7 +21,22 @@ except Exception as err:
              "to current working directory.")
 
 
+def vram_usage():
+    """ Log and return the vram usage.
+    Returns:
+        current usage, total vram
+    """
+    mem = torch.cuda.mem_get_info()
+    o = mem[0]/1024/1024/1024
+    t = mem[1]/1024/1024/1024
+    log.info("VRAM usage: %0.4f GB / %0.4f GB" %(o, t))
+    return o, t
+
+
 def unload_model():
+    """ Attempt to clear the momery by unloading any
+    existing model.
+    """
     if polyai.server.model is not None:
         import gc, torch
         t1 = log.trace("Unloading current model.")
@@ -31,17 +47,25 @@ def unload_model():
         t1.done("Unloaded current model.")
 
 
-def init_gptq_model(modelname, use_fast=False):
+def init_gptq_model(modelname, groupsize=-1, fused_mlp=False, use_fast=False):
+    """
+    Load a pretrained 4 bit GPTQ model using GPTQ-for-LLaMa.
+    Arguments:
+        groupsize int : GPTQ group size, default -1.
+        use_fast bool : Use fast tokenizer.
+        fused_mlp bool : Use fused mlp or not, use false if model loading fails.
+    """
     unload_model()
     t1 = log.trace("Loading GPTQ model: {}", modelname)
 
     modeldir = os.path.dirname(modelname)
-    polyai.server.model = gptq.load_quant(modeldir, modelname, 4, -1, fused_mlp=False)
+    polyai.server.model = gptq.load_quant(modeldir, modelname, 4, groupsize, fused_mlp=fused_mlp)
     polyai.server.model.to(gptq.DEV)
     polyai.server.token = gptq.AutoTokenizer.from_pretrained(modeldir, use_fast=use_fast)
     polyai.server.modelName = os.path.basename(modelname)
 
     t1.done("Model loaded: {}", modelname)
+    vram_usage()
 
 
 def init_hf_model(pipeline_type, modelname):
