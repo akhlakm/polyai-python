b93f634c58f088083b2b55d3f04a6250e87271d9
akhlakm <me@akhlakm.com>
2023-07-04 20:26:45 -0400

    Update memory usage script

--- scripts/memory_usage.py
+++ scripts/memory_usage.py
@@ -1,6 +1,11 @@
 import torch
 
-mem = torch.cuda.mem_get_info()
-free = mem[0]/1024/1024/1024
-total = mem[1]/1024/1024/1024
-print("VRAM usage: %0.4f GB / %0.4f GB (%0.4f GB free)" %(total-free, total, free))
+free = 0
+total = 0
+for i in range(torch.cuda.device_count()):
+    mem = torch.cuda.mem_get_info(i)
+    free += mem[0]/1024/1024/1024
+    total += mem[1]/1024/1024/1024
+
+print("Cuda devices:", torch.cuda.device_count())
+print("Total VRAM usage: %0.4f GB / %0.4f GB (%0.4f GB free)" %(total-free, total, free))
