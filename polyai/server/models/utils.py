e53eb9eb23d5f15431f212ddbb7b4ac9c48dcfb1
akhlakm <me@akhlakm.com>
2023-07-04 20:34:01 -0400

    Update vram_usage()

--- polyai/server/models/utils.py
+++ polyai/server/models/utils.py
@@ -19,8 +19,12 @@ def vram_usage():
     Returns:
         current usage, total vram, free vram
     """
-    mem = torch.cuda.mem_get_info()
-    free = mem[0]/1024/1024/1024
-    total = mem[1]/1024/1024/1024
-    log.info("VRAM usage: %0.4f GB / %0.4f GB (%0.4f GB free)" %(total-free, total, free))
+    free = 0
+    total = 0
+    devices = torch.cuda.device_count()
+    for i in range(devices):
+        mem = torch.cuda.mem_get_info(i)
+        free += mem[0]/1024/1024/1024
+        total += mem[1]/1024/1024/1024
+    log.info("VRAM usage, %d GPUs: %0.4f GB / %0.4f GB (%0.4f GB free)" %(devices, total-free, total, free))
     return total - free, total, free
