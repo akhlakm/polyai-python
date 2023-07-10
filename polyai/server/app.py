4794c1b5bde89580a250dfcd04914c8b30f3125e
akhlakm <me@akhlakm.com>
2023-07-09 20:41:05 -0400

    Add /v1 suffix to the api url

--- polyai/server/app.py
+++ polyai/server/app.py
@@ -4,6 +4,6 @@ def run(host, port, debug):
     app = Flask(__name__, static_url_path = "")
 
     from . import api_v1 as api
-    app.register_blueprint(api.bp, url_prefix="/api")
+    app.register_blueprint(api.bp, url_prefix="/api/v1")
 
     app.run(host=host, port=port, debug=debug)
