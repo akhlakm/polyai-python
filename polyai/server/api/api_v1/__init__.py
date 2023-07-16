1b222a28e9f3fec7bac37768f0ba8638a82ce760
akhlakm <me@akhlakm.com>
2023-07-16 16:12:50 -0400

    fix url prefix

--- polyai/server/api/api_v1/__init__.py
+++ polyai/server/api/api_v1/__init__.py
@@ -5,5 +5,5 @@ def run(port, listen=False, debug=False):
     """ Run api/v1 server (openai like). """
     host = '0.0.0.0' if listen else '127.0.0.1'
     app = Flask(__name__, static_url_path = "")
-    app.register_blueprint(blocking.bp, url_prefix="/api/v1")
+    app.register_blueprint(blocking.bp, url_prefix="/api/v1/")
     app.run(host=host, port=port, debug=debug)
