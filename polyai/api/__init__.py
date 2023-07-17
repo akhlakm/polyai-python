c28dd984d6980287f1ed51788bad53436d294f0e
akhlakm <me@akhlakm.com>
2023-07-16 21:14:03 -0400

    Update url

--- polyai/api/__init__.py
+++ polyai/api/__init__.py
@@ -6,7 +6,7 @@ from .util import create_ssh_tunnel
 
 api_key = os.environ.get("POLYAI_API_KEY")
 api_key_path = os.environ.get("POLYAI_API_KEY_PATH")
-api_base = os.environ.get("POLYAI_API_BASE", "http://localhost:8080/api/v1/")
+api_base = os.environ.get("POLYAI_API_BASE", "http://localhost:8001/polyai/")
 
 session = None
 app_info = None
